115fb8baee6128b9d4c51f41127efda6f129885b
Linn Johansen <linn.es.johansen@gmail.com>
2025-01-07 19:51:36 -0800

    Quiz progressbar done

--- src/components/quiz/Progress.jsx
+++ src/components/quiz/Progress.jsx
@@ -1,13 +1,23 @@
 function Progress({ index, numQuestions, answer, points }) {
   return (
-    <div className='flex items-center gap-4 w-full'>
+    <div className='flex justify-center items-center gap-4 mx-10'>
+      {/* Progress bar */}
       <progress
-        className='w-full h-8'
         max={numQuestions}
         value={index + Number(answer !== null)}
+        aria-valuenow={index + Number(answer !== null)}
+        aria-valuemin='0'
+        aria-valuemax={numQuestions}
+        aria-label={`Progress ${
+          index + Number(answer !== null)
+        } of ${numQuestions}`}
       ></progress>
-      <p className='min-w-fit'>{points} points</p>
-      {/* <p>{`${index + 1}/${numQuestions}`}</p> */}
+
+      {/* Points counter */}
+      <p className='flex gap-1 min-w-fit text-lg '>
+        <span className='font-bold'>{`${points}/10`}</span>
+        points
+      </p>
     </div>
   );
 }
