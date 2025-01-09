65636c7ca165c1d84031056c8a7fac4fd6d4d202
Linn Johansen <linn.es.johansen@gmail.com>
2025-01-09 10:11:38 -0800

    Refactoring CSS: quiz done

--- src/components/quiz/StartScreen.jsx
+++ src/components/quiz/StartScreen.jsx
@@ -1,14 +1,14 @@
 import { useQuiz } from '../../contexts/QuizContext';
 import Button from '../common/Button';
+import styles from './StartScreen.module.css';
 
 function StartScreen() {
   const { dispatch, numQuestions, SECONDS_PER_QUESTION: seconds } = useQuiz();
 
   return (
-    <div className='w-full flex items-center gap-4'>
-      <h3 className='w-1/4'>Welcome to the Array Quiz!</h3>
-
-      <div className='flex flex-col'>
+    <div className={styles.startScreen}>
+      <h3>Welcome to the Array Quiz!</h3>
+      <div className={styles.startContent}>
         <p>{`There's a total of ${numQuestions} questions, and you will have ${seconds} seconds to solve each one. Good luck! 🤞`}</p>
 
         <Button onClick={() => dispatch({ type: 'start' })}>
