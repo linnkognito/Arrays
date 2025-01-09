9a8492e5350f0a28e282b63be1df5cc392ab6ddd
Linn Johansen <linn.es.johansen@gmail.com>
2025-01-08 20:41:08 -0800

    Refactoring CSS: methods done

--- src/components/methods/Method.jsx
+++ src/components/methods/Method.jsx
@@ -1,23 +1,30 @@
 import Code from '../challenges/Code';
+import styles from './Method.module.css';
+import classNames from 'classnames';
 
 function Method({ method, description, syntax, returns, accepts }) {
   return (
-    <div className='method'>
-      <div className='method-name'>{method}</div>
-
-      <p className='method-description'>{description}</p>
-
+    <div className={styles.methodCard}>
+      <h3>{method}</h3>
+      <p>{description}</p>
       <Code>{syntax}</Code>
 
-      <div className='method-accepts clues-container clue'>
-        <p>
-          <span className='method-title'>Accepts:</span> {accepts || '-'}
-        </p>
-      </div>
-      <div className='method-returns clues-container clue'>
-        <p>
-          <span className='method-title'>Returns:</span> {returns || '-'}
-        </p>
+      <div className={styles.acceptsReturnsWrapper}>
+        {/* Accepts */}
+        <div className={classNames(styles.methodAccepts)}>
+          <p>
+            <span className={styles.methodSubTitle}>Accepts:</span>{' '}
+            {accepts || '-'}
+          </p>
+        </div>
+
+        {/* Returns */}
+        <div className={classNames(styles.methodReturns)}>
+          <p>
+            <span className={styles.methodSubTitle}>Returns:</span>{' '}
+            {returns || '-'}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/methods/MethodCheatSheet.jsx
+++ src/components/methods/MethodCheatSheet.jsx
@@ -1,20 +1,24 @@
 import Method from './Method';
+import styles from './MethodCheatSheet.module.css';
 
 function MethodCheatSheet({ methodList }) {
   return (
-    <div className='methods'>
-      <h2>Methods cheat sheet</h2>
+    <>
+      <div className={styles.methods}>
+        <h2>Methods cheat sheet</h2>
 
-      {methodList?.map((method) => (
-        <Method
-          method={method.name}
-          description={method.description}
-          syntax={method.syntax}
-          returns={method.returns}
-          accepts={method.accepts}
-        />
-      ))}
-    </div>
+        {methodList?.map((method) => (
+          <Method
+            key={method.name}
+            method={method.name}
+            description={method.description}
+            syntax={method.syntax}
+            returns={method.returns}
+            accepts={method.accepts}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 export default MethodCheatSheet;
